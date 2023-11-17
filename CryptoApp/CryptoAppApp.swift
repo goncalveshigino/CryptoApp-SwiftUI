e39ca520244ba81b71d3e1d5c78bcad92b7ddc4d
goncalveshigino <goncalveshigino20@gmail.com>
2023-11-17 13:38:07 +0100

    Criando CryptoApp

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Goncalves Higino on 17/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CryptoApp/Extensions/Color.swift
@@ -0,0 +1,26 @@
+//
+//  Color.swift
+//  CryptoApp
+//
+//  Created by Goncalves Higino on 17/11/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let theme = ColorTheme()
+    
+}
+
+
+struct ColorTheme {
+    
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+    
+}
